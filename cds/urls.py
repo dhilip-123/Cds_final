d5e4402036fcf3dc9066e93704b85b0f87343182
amanvivekanand006 <amanamn2000@gmail.com>
2024-10-16 16:29:57 +0530

    setting

---
+++ cds/urls.py
@@ -0,0 +1,58 @@
+"""
+URL configuration for cds project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view 
+from drf_yasg import openapi 
+from rest_framework.routers import DefaultRouter
+from community.views import *
+from election_officer.views import *
+from vote.views import *
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Community Development System API",
+        default_version='v1',
+        description="API documentation for the Community Development System.",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+router = DefaultRouter()
+router.register(r'communities', CommunityViewSet)
+router.register(r'users', UserViewSet)
+router.register(r'community-admins', CommunityAdminViewSet)
+router.register(r'election-officers', ElectionOfficerViewSet)
+router.register(r'contestants', ContestantViewSet)
+router.register(r'contests', ContestViewSet)
+router.register(r'voting_members', VotingMemberViewSet)
+router.register(r'election_schedule', ElectionScheduleViewSet)
+router.register(r'candidates', CandidateViewSet, basename='candidate')
+router.register(r'votes', VoteViewSet, basename='vote')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)), 
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
